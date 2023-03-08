124c1d33b00f696e8090e5be4cbb41a3657a4ff3
Michaella Rodriguez <micha.ella@hotmail.com>
2023-03-08 10:34:16 -0600

    fully functional

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const keypad = document.querySelector('.keypad');
+const keys = document.querySelectorAll('.key');
 
-const keys = document.querySelectorAll('.key')
-
-const phoneDisplay = document.querySelector(".phone-display")
-const pagerDisplay = document.querySelector(".pager-display")
+const phoneDisplay = document.querySelector(".phone-display");
+const pagerDisplay = document.querySelector(".pager-display");
 
 keys.forEach(key => {
     key.addEventListener('click', () => handleClick(key.id));
 })
 
 function handleClick(id) {
-    phoneDisplay.textContent += id;
+    let number = phoneDisplay.textContent;
+    if (id === "reset") {
+        pagerDisplay.textContent = "";
+        phoneDisplay.textContent = "";
+    } else if (id === "send") {
+        phoneDisplay.textContent = "";
+        pagerDisplay.textContent = number;
+        youWerePaged();
+    } else {
+        phoneDisplay.textContent += id;
+    }
 };
+
+const youWerePaged = () => new Audio("assets/pager.wav").play();
