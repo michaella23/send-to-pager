41a33ba3bb8ecae7b6ba1af74fdebdf9ccabb61c
Michaella Rodriguez <micha.ella@hotmail.com>
2023-03-07 21:09:34 -0600

    pseudocode for rendering keypad

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 // create array with keys
 // use forEach to 
-// - create element
+// - create element (button)
 // - append to keypad
+// - give it text content
+
+// const keypad = document.querySelector('keypad');
+
+// const keys = [
+//     '1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#', 'RESET', 'SEND'
+// ]
+
+// keys.forEach(key => {
+//     const buttonEl = document.createElement('button');
+//     buttonEl.textContent = key;
+//     keypad.append(buttonEl);
+// })
